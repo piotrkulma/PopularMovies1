851c0e6cae6cd1e5636fe55e8d1ac186177df275
Piotr Kulma <piotrkulma@gmail.com>
2017-02-06 22:12:55 +0100

    -Refactoring movies details layout into constraints layout -Binding data from the constrainst layout

--- app/src/main/java/pl/piotrkulma/popularmoviesstage1/MovieActivity.java
+++ app/src/main/java/pl/piotrkulma/popularmoviesstage1/MovieActivity.java
@@ -1,15 +1,16 @@
 package pl.piotrkulma.popularmoviesstage1;
 
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
-import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import pl.piotrkulma.popularmoviesstage1.databinding.ActivityMovieDetailsBinding;
 import pl.piotrkulma.popularmoviesstage1.model.MovieDBResponse;
 
 /**
@@ -17,63 +18,54 @@ import pl.piotrkulma.popularmoviesstage1.model.MovieDBResponse;
  *
  */
 public class MovieActivity extends AppCompatActivity {
-    private TextView title;
-    private TextView releaseDate;
     private ImageView poster;
-    private TextView avgVotes;
-    private TextView synopsis;
     private ProgressBar progressBar;
+
+    private ActivityMovieDetailsBinding movieDetailsBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_movie);
-
-        initChildViews();
-
-        Intent intentFromParent = getIntent();
-
-        MovieDBResponse movieData = (MovieDBResponse)intentFromParent.getSerializableExtra("movieData");
-
-        fillMovieData(movieData);
+        initViews();
+        bindData();
     }
 
-    private void initChildViews() {
-        title = (TextView) findViewById(R.id.movie_title);
-        releaseDate = (TextView) findViewById(R.id.movie_release_date);
-        poster = (ImageView) findViewById(R.id.movie_poster);
-        avgVotes = (TextView) findViewById(R.id.movie_avg_votes);
-        synopsis = (TextView) findViewById(R.id.movie_synopsis);
+    private void initViews() {
+        movieDetailsBinding = DataBindingUtil.setContentView(this, R.layout.activity_movie_details);
+        poster = (ImageView) findViewById(R.id.poster);
         progressBar = (ProgressBar) findViewById(R.id.movie_progressbar);
     }
 
-    private void fillMovieData(MovieDBResponse movieData) {
+    private void bindData() {
         hideAll();
+        Intent intentFromParent = getIntent();
 
-        title.setText(movieData.getTitle());
-        releaseDate.setText(movieData.getReleaseDateYear());
-        avgVotes.setText(movieData.getVoteAgerageFull());
-        synopsis.setText(movieData.getPlotSynopsis());
+        MovieDBResponse movieData = (MovieDBResponse)intentFromParent.getSerializableExtra("movieData");
+        movieDetailsBinding.setMovie(movieData);
 
         Picasso.with(MovieActivity.this).load(movieData.getPosterUrl()).into(poster);
         showAll();
     }
 
+    private void fillMovieData(MovieDBResponse movieData) {
+        //hideAll();
+/*
+        title.setText(movieData.getTitle());
+        releaseDate.setText(movieData.getReleaseDateYear());
+        avgVotes.setText(movieData.getVoteAgerageFull());
+        synopsis.setText(movieData.getPlotSynopsis());
+
+        Picasso.with(MovieActivity.this).load(movieData.getPosterUrl()).into(poster);*/
+        //showAll();
+    }
+
     private void showAll() {
-        title.setVisibility(View.VISIBLE);
-        releaseDate.setVisibility(View.VISIBLE);
         poster.setVisibility(View.VISIBLE);
-        avgVotes.setVisibility(View.VISIBLE);
-        synopsis.setVisibility(View.VISIBLE);
         progressBar.setVisibility(View.INVISIBLE);
     }
 
     private void hideAll() {
-        title.setVisibility(View.INVISIBLE);
-        releaseDate.setVisibility(View.INVISIBLE);
         poster.setVisibility(View.INVISIBLE);
-        avgVotes.setVisibility(View.INVISIBLE);
-        synopsis.setVisibility(View.INVISIBLE);
         progressBar.setVisibility(View.VISIBLE);
     }
 }
